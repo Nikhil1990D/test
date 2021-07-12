6ccee05c9d3a2096b7576be2335ca3c2f58496be
Nikhil S Dhabekar <Nikhil S Dhabekar@RGI-CEM-L031>
2021-07-12 23:29:27 +0530

    commitm

---
+++ Swap/src/Swap1/Swap2.java
@@ -0,0 +1,16 @@
+package Swap1;
+
+public class Swap2 
+{
+	public static void main(String[] args) 
+	{
+		int a=6 ;
+		int b=3;
+		int c;
+			c=a;
+			a=b;
+			b=c;
+			System.out.println("a is "+a);
+			System.out.println("b is "+b);
+	}
+}
